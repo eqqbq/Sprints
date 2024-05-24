4974aa9581bca48ee8bb63f43b552f445b093e46
eqqbq <160983984+eqqbq@users.noreply.github.com>
2024-05-24 22:03:23 +0200

    Update Sprint 4

---
+++ src/main/java/Sprint4/S04T01N02UrpinaDavid/src/main/java/cat/itacademy/barcelonactiva/Urpina/David/s04/t01/n02/S04T01N02UrpinaDavid/S04T01N02UrpinaDavidApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Urpina.David.s04.t01.n02.S04T01N02UrpinaDavid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N02UrpinaDavidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N02UrpinaDavidApplication.class, args);
+	}
+
+}
---
+++ src/main/java/Sprint4/S04T01N02UrpinaDavid/src/test/java/cat/itacademy/barcelonactiva/Urpina/David/s04/t01/n02/S04T01N02UrpinaDavid/S04T01N02UrpinaDavidApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Urpina.David.s04.t01.n02.S04T01N02UrpinaDavid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N02UrpinaDavidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
